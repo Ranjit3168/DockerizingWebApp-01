c1a12f48227eea8d83e7b9e766fb43a114c15dd6
Ranjit3168 <ranjit.sahoo@bluepal.com>
2024-05-10 18:18:17 +0530

    initial commit

---
+++ src/main/java/com/bpl/BootProj01DockerizingApp01Application.java
@@ -0,0 +1,13 @@
+package com.bpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj01DockerizingApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj01DockerizingApp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpl/BootProj01DockerizingApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj01DockerizingApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
